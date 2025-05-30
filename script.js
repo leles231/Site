bc28ed8388ed4e3dff2247e0e943feef617072be
leles231 <raquelleles231@gmail.com>
2025-05-30 10:22:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ const hamburger = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar');
+
+    hamburger.addEventListener('click', () => {
+      sidebar.classList.toggle('active');
+    });
+    
+       
